afee479af5d12e4bc4ca67587a3dc292612b7ce5
Marghrid <margarida@cmu.edu>
2025-05-20 12:52:37 -0400

    Add embedding_NLP definition to run_hyglad_dataset.py

--- run_hyglad_dataset.py
+++ run_hyglad_dataset.py
@@ -7,11 +7,68 @@ import time
 from datetime import datetime
 
 import jsonlines
+import matplotlib.pyplot as plt
 import numpy as np
+import torch
+from sklearn.manifold import TSNE
 from sklearn.metrics import precision_score, recall_score
+from torch.utils.data import DataLoader, TensorDataset
+from tqdm import tqdm
+# model
+# tokenizer
+from transformers import RobertaModel, RobertaTokenizer
 
-from DeepOD.compute_embedding_for_beth import embedding_NLP
 from deepod.models.tabular import DeepIsolationForest, DeepSVDD, GOAD, ICL, NeuTraL, RCA, RDP, REPEN, SLAD
+
+
+def embedding_NLP(text=None, label=None, max_len=512, save=True, plot=True, save_name=None,
+                  cuda=torch.cuda.is_available()):
+    # extract embedding vector from the pretrained NLP model
+
+    # tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+    # encoder = BertModel.from_pretrained("bert-base-uncased")
+
+    tokenizer = RobertaTokenizer.from_pretrained("roberta-base")
+    encoder = RobertaModel.from_pretrained("roberta-base")
+
+    if cuda:
+        encoder.cuda()
+    encoder.eval()
+
+    encoding = tokenizer(list(text), return_tensors='pt', padding=True, truncation=True)
+    input_ids = encoding['input_ids'];
+    input_ids = input_ids[:, :max_len]
+    attention_mask = encoding['attention_mask'];
+    attention_mask = attention_mask[:, :max_len]
+    dataloader = DataLoader(TensorDataset(input_ids, attention_mask), shuffle=False, batch_size=8, drop_last=False)
+
+    # The embedding see https://github.com/huggingface/transformers/issues/7540
+    embedding_list = []
+
+    with torch.no_grad():
+        for batch in tqdm(dataloader):
+            if cuda:
+                batch = tuple(d.cuda() for d in batch)
+            input, mask = batch
+            embedding = encoder(input_ids=input, attention_mask=mask)
+            # embedding = torch.mean(embedding.last_hidden_state, dim=1)
+            embedding = embedding.pooler_output
+            embedding_list.append(embedding.cpu().numpy())
+
+    X = np.vstack(embedding_list)
+    y = np.array(label)
+
+    np.savez_compressed(os.path.join('', save_name + '.npz'), X=X, y=y)
+
+    if plot:
+        # visualization
+        X_tsne = TSNE(n_components=2, random_state=42).fit_transform(X[:1000])
+        y_tsne = np.array(y[:1000])
+
+        plt.scatter(X_tsne[y_tsne == 0, 0], X_tsne[y_tsne == 0, 1], color='blue')
+        plt.scatter(X_tsne[y_tsne == 1, 0], X_tsne[y_tsne == 1, 1], color='red')
+        plt.title(save_name)
+        plt.show()
 
 
 def run_model_for_presplit_dataset(model, train_npz_file, test_npz_file, task_idx=None):
